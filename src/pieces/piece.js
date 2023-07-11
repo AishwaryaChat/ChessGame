25cac7012a217b0d18d2090b547eaa20747c29bf
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-07-11 22:43:46 +0530

    Add Piece class

---
+++ src/pieces/piece.js
@@ -0,0 +1,9 @@
+export default class Piece {
+    constructor(player, iconURL) {
+        this.player = player
+        this.style = {backgroundImage: `url(${iconURL})`}
+    }
+    getPlayer() {
+        return this.player
+    }
+}
